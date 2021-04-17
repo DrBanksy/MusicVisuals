24f9325a2394b456fbc486a8738c452e0598cb68
cormac smith <cormacsmith@live.co.uk>
2021-04-17 12:59:38 +0100

    update readMe

--- java/src/C19313793/WelcomeScreen.java
+++ java/src/C19313793/WelcomeScreen.java
@@ -2,8 +2,6 @@ package C19313793;
 
 public class WelcomeScreen {
     private MyDesign m;
-    private float s = 100 + (100 * 10 * 10);
-    private float angle = 0;
 
     WelcomeScreen(MyDesign mydesign) {
         this.m = mydesign;
@@ -17,10 +15,4 @@ public class WelcomeScreen {
         m.text("2) Use 0-9 on the keyboard", m.width/2, m.height/2 + 90);
         m.text("WARNING: Loud headbanger music incoming", m.width/2, m.height/2 + 160);
     }
-
-
-
-    
-
-    
 }
